76e3c6d4fcb29e3312db41223e3e76360ad4503d
umar51 <66181738+umar51@users.noreply.github.com>
2020-06-07 00:01:08 +0500

    bm hj

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
+startimport React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div> Hello World</div>
   document.getElementById('root')
 );
 
