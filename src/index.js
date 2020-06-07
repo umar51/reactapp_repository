5297b31396f8c91f44930774f03d8c4165d3d508
umar51 <66181738+umar51@users.noreply.github.com>
2020-06-08 00:56:35 +0500

    new changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
 import MediaCard from './MediaCard';
-import Gate from './Gate';
+//import Gate from './Gate';
 
 ReactDOM.render(
-  <div><Gate isOpen= {false} /></div>,
+  //<div><Gate isOpen= {false} /></div>,
   <div><MediaCard title="My Assignment" body="This is the first assignment of react app tutorial" imageUrl="https://media.gettyimages.com/photos/wazir-khan-mosque-lahore-punjab-pakistan-picture-id637623678?s=2048x2048"/></div>,
  // <div><App name = "Umar" age = {25}/></div>,
   //React.createElement("div", null, React.createElement("span", null, "Hello from create element inside create element")),
